67db6e420c81d8b6531a5b9491406d9007ebe278
Nikita Mochalov <mochalov.n@yandex.ru>
2018-06-08 21:04:32 +0300

    non-local server, udp

--- production/tcpclient.py
+++ production/tcpclient.py
@@ -4,7 +4,7 @@ import socket
 import snakesockets
 
 client_socket = snakesockets.TCP()
-client_socket.connect( ("localhost", 9000) )
+client_socket.connect( ("80.211.205.161", 9000) )
 client_socket.send("привет от клиента {}!".format(sys.argv[1]))
 data = client_socket.recv()
 print(data)
