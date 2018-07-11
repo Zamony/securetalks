f48c59a4f5c5fd473ec37d0b04930304e3a7a10f
Nikita Mochalov <mochalov.n@yandex.ru>
2018-07-11 20:59:35 +0300

    orm methoods changed

--- securetalks/storage.py
+++ securetalks/storage.py
@@ -9,7 +9,8 @@ class Nodes:
         self._conn = conn
         self._cursor = cursor
 
-    def update_node(self, node):
+    def update_node_activity(self, node):
+        node.update_activity()
         self._cursor.execute("""
         UPDATE `Nodes` SET `last_activity`=? WHERE `node_id`=?
         """,
@@ -18,8 +19,19 @@ class Nodes:
         self._conn.commit()
 
     def increment_node_unread(self, node):
+        node.increment_unread()
         self._cursor.execute("""
         UPDATE `Nodes` SET `unread_count` = `unread_count`+1
+        WHERE `node_id`=?
+        """,
+        (node.node_id,)
+        )            
+        self._conn.commit()
+
+    def set_node_unread_to_zero(self):
+        node.set_unread_to_zero()
+        self._cursor.execute("""
+        UPDATE `Nodes` SET `unread_count` = 0
         WHERE `node_id`=?
         """,
         (node.node_id,)
@@ -36,24 +48,27 @@ class Nodes:
         node_exists, = self._cursor.fetchone()
         return True if node_exists else False
 
-    def add(self, node):
+    def add_node(self, node):
         self._cursor.execute(
-        "INSERT INTO `Nodes` VALUES (?, ?, 0)",
-        (node.node_id, node.last_activity, node.unread_count))
+        "INSERT INTO `Nodes` VALUES (?, ?, ?, ?)",
+        (
+            node.node_id, node.last_activity,
+            node.unread_count, node.alias
+        )
         )
         self._conn.commit()
 
-    def delete(self, node):
+    def delete_node(self, node):
         self._cursor.execute(
         "DELETE FROM `Nodes` WHERE `node_id`=?",
         (node.node_id,)
         )
         self._conn.commit()
 
-    def get(self, node):
+    def get_node_by_id(self, node_id):
         self._cursor.execute(
             "SELECT * FROM `Nodes` WHERE `node_id`=?",
-            (node.node_id, )   
+            (node_id, )
         )
 
         node = self._cursor.fetchone()
@@ -72,7 +87,7 @@ class Messages:
         self._conn = conn
         self._cursor = cursor
 
-    def add(self, message):
+    def add_message(self, message):
         self._cursor.execute(
             "INSERT INTO `Messages` VALUES (?, ?, ?, ?)",
             (
@@ -82,9 +97,9 @@ class Messages:
                 message.timestamp
             )
         )
-        self.conn.commit()
+        self._conn.commit()
 
-    def get(self, node, limit=None, offset=None):
+    def get_messages(self, node, limit=None, offset=None):
         if limit is not None and offset is not None:
             self._cursor.execute(
             """
@@ -112,17 +127,14 @@ class Messages:
         else:
             raise sqlite3.Error
 
-        for node_id, text, to_me, timestamp in self._cursor.fetchone():
-            yield orm.Message(
-                node_id,
-                text,
-                True if to_me else False,
-                timestamp
-            )
+        return [
+            orm.Message(nid, txt, True if to_me else False, tm)
+            for nid, txt, to_me, tm in self._cursor.fetchall()
+        ]
 
-    def delete(self, node):
+    def delete_message(self, node):
         self._cursor.execute(
-            "DELETE FROM `Messages` WHERE `node_id`=?,
+            "DELETE FROM `Messages` WHERE `node_id`=?",
             (node.node_id,)
         )
         self._cursor.commit()
@@ -139,7 +151,7 @@ class Ciphergrams:
         )
         self._conn.commit()
 
-    def add(self, ciphergram):
+    def add_ciphergram(self, ciphergram):
         self._cursor.execute(
             "INSERT INTO `Ciphergrams` VALUES (?, ?)",
             (ciphergram.content, ciphergram.timestamp)
@@ -162,7 +174,7 @@ class IPAddresses:
         )
         self._conn.commit()
 
-    def add(self, ipaddress):
+    def add_ipaddress(self, ipaddress):
         self._cursor.execute(
             "INSERT INTO `IPAddresses` VALUES (?, ?)",
             (ipaddress.address, ipaddress.last_activity)
@@ -170,6 +182,7 @@ class IPAddresses:
         self._conn.commit()
 
     def update_ipaddress(self, ipaddress):
+        ipaddress.update_activity()
         self._cursor.execute(
             """
             UPDATE `Nodes` SET `last_activity`=? WHERE `address`=?
@@ -191,97 +204,18 @@ class Storage:
 
         self.nodes = Nodes(self._conn, self._cursor)
         self.messages = Messages(self._conn, self._cursor)
-        self.ciphergrams = Ciphergrams(self._conn, self._cursor, self._ttl)
-        self.ipaddresses = IPAddresses(self._conn, self._cursor, self._ttl)
+        self.ciphergrams = Ciphergrams(self._conn, self._cursor)
+        self.ipaddresses = IPAddresses(self._conn, self._cursor)
 
-    @property
-    def known_addresses(self):
-        self.cursor.execute("SELECT `ip_address` FROM `ip_addresses`")
-        return (ip for ip, in self.cursor.fetchall())
+    def __enter__(self):
+        return self
 
-    def add_known_address(self, ip_address):
-        self.cursor.execute(
-            "INSERT INTO `ip_addresses` VALUES (?, ?)", (ip_address, int(time.time()))
-        )
-        self.conn.commit()
-
-    @property
-    def ciphergrams(self):
-        self.cursor.execute("SELECT `ciphergram`, `timestamp` FROM `ciphergrams`")
-        return cursor.fetchall()
-
-    def add_ciphergram(self, ciphergram):
-        self.cursor.execute(
-            "INSERT INTO `ciphergrams` VALUES (?, ?)", (ciphergram, int(time.time()))
-        )
-        self.conn.commit()
-
-    @property
-    def dialogs(self):
-        self.cursor.execute("""
-        SELECT `dialog_with`, `last_activity`, `unread_count` FROM `dialogs`
-        """)
-        return cursor.fetchall()
-
-    def add_dialog(self, uid):
-        self.cursor.execute(
-            "INSERT INTO `dialogs` VALUES (?, ?, 0)",
-            (uid, int(time.time()))
-        )
-        self.conn.commit()
-
-    def increase_dialog_unread(self, uid, count=1):
-        self.cursor.execute("""
-        UPDATE `dialogs` SET `unread_count` = `unread_count`+?
-        WHERE `dialog_with`=?
-        """, (count, uid))
-        self.conn.commit()
-
-    def set_dialog_unrad_to_zero(self, uid):
-        self.cursor.execute("""
-        UPDATE `dialogs` SET `unread_count` = 0 WHERE `dialog_with`=?
-        """, (uid,))
-        self.conn.commit()
-
-    def update_dialog_activity(self, uid):
-        self.cursor.execute(
-            "UPDATE `dialogs` SET `last_activity`=? WHERE `dialog_with`=?",
-            (int(time.time()), uid)
-        )
-        self.conn.commit()
-
-    def get_messages_from(self, who, limit=None, offset=None):
-        if limit is not None and offset is not None:
-            self.cursor.execute("""
-            SELECT `text`, `from_me`, `timestamp` FROM `messages`
-            WHERE `dialog_with`=? ORDER BY `timestamp` DESC LIMIT ? OFFSET ?;
-            """, (who, limit, offset))
-        elif limit is None and offset is None:
-            self.cursor.execute("""
-            SELECT `text`, `from_me`, `timestamp` FROM `messages`
-            WHERE `dialog_with`=? ORDER BY `timestamp` DESC;
-            """, (who,))
-        elif limit is not None:
-            self.cursor.execute("""
-            SELECT `text`, `from_me`, `timestamp` FROM `messages`
-            WHERE `dialog_with`=? ORDER BY `timestamp` DESC LIMIT ?;
-            """, (who, limit))
-        else:
-            raise sqlite3.Error
-
-        return self.cursor.fetchall()
-
-    def add_message_from(who, text):
-        self.cursor.execute(
-            "INSERT INTO `messages` VALUES (?, ?, ?)",
-            (who, text, int(time.time()))
-        )
-        self.conn.commit()        
-        
+    def __exit__(self, type, value, traceback):
+        self._conn.close()
 
     def _create_tables_if_needed(self):
         try:
-            self.cursor.executescript(
+            self._cursor.executescript(
             """
             CREATE TABLE `IPAddresses` (
                 `address`	TEXT NOT NULL,
@@ -311,23 +245,13 @@ class Storage:
             pass
 
     def delete_old_data(self):
-        curr_time = int(time.time())
-        self.cursor.execute(
-            "DELETE FROM `ciphergrams` WHERE ? - `timestamp` > ?", (curr_time, self.ttl)
-        )
-        self.cursor.execute(
-            "DELETE FROM `ip_addresses` WHERE ? - `timestamp` > ?",
-            (curr_time, self.ttl),
-        )
-        self.conn.commit()
-
-    def close(self):
-        self.conn.close()
+        self.ciphergrams.delete_old_ones(self._ttl)
+        self.ipaddresses.delete_old_ones(self._ttl)
 
 
 if __name__ == "__main__":
     db_path = pathlib.Path.home() / ".securetalks" / "db.sqlite3"
-    seconds_in_two_days = 60 * 60 * 24 * 2
-    storage = Storage(db_path, seconds_in_two_days)
-    storage.update_dialog_activity("b")
-    storage.close()
+    two_days_secs = 60 * 60 * 24 * 2
+
+    with Storage(db_path, two_days_secs) as storage:
+        pass
