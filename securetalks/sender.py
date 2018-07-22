e1c2accc405acc86ea2bbfa7003a969ef87157f9
Nikita Mochalov <mochalov.n@yandex.ru>
2018-07-22 17:57:44 +0300

    Added request_offline_data functionality

--- securetalks/sender.py
+++ securetalks/sender.py
@@ -1,13 +1,16 @@
 import json
+import dataclasses
 import multiprocessing
 
 from . import crypto
 from . import snakesockets
 
+
 class Sender:
-    def __init__(self, storage, mcrypto):
+    def __init__(self, storage, mcrypto, queue):
+        self.queue = queue
         self.storage = storage
-        self.queue = multiprocessing.Queue()
+        self.offline_requested = None
         self.llsender = LowLevelSender(self.queue, mcrypto)
         self.llsender_proc = multiprocessing.Process(
             target=self.llsender.run
@@ -16,10 +19,18 @@ class Sender:
 
     def send_message_to(self, user_key, message):
         self.broadcast(message, user_key)
-    
+
     def request_offline_data(self):
+        self.offline_requested = {
+            address for address in self.storage.ipaddresses.listall()
+        }
         self.broadcast(
             json.dumps(dict(type="request_offline_data"))
+        )
+
+    def send_to(self, message, ip_address):
+        self.queue.put(
+            ([ip_address, ], message, None)
         )
 
     def broadcast(self, message, user_key=None):
@@ -35,6 +46,7 @@ class Sender:
         self.llsender_proc.terminate()
         self.llsender_proc.join()
 
+
 class LowLevelSender:
     def __init__(self, queue, mcrypto):
         self.queue = queue
@@ -43,7 +55,7 @@ class LowLevelSender:
     def _send_message(self, ip_addresses, message):
         for ip_address in ip_addresses:
             client_socket = snakesockets.TCP()
-            client_socket.connect( (ip_address.address, 8080) )
+            client_socket.connect((ip_address.address, 8080))
             client_socket.send(message)
 
     def run(self):
@@ -51,6 +63,14 @@ class LowLevelSender:
             addresses, message, node_id = self.queue.get()
             if node_id is not None:
                 ciphergram = self.mcrypto.get_ciphergram(node_id, message)
-                self._send_message(addresses, ciphergram)
+                self._send_message(
+                    addresses,
+                    json.dumps(
+                        dict(
+                            type="ciphergram",
+                            **dataclasses.asdict(ciphergram)
+                        )
+                    )
+                )
             else:
                 self._send_message(addresses, message)
