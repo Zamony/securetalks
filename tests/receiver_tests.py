95f05d7aa80aa8dd392838b0a35f084e0e8eb41f
Nikita Mochalov <mochalov.n@yandex.ru>
2018-07-22 23:23:39 +0300

    Refactored receiver_tests.py

--- tests/receiver_tests.py
+++ tests/receiver_tests.py
@@ -25,45 +25,36 @@ class TestReceiver(unittest.TestCase):
         self.recver_mcrypto = crypto.MessageCrypto(
             self.recver_keys, 60*60
         )
-        self.receiver = None
+        ciphergram = self.sender_mcrypto.get_ciphergram(
+            self.recver_keys.pub_key_str, "Message from sender"
+        )
+        self.message = dict(
+            type="ciphergram",**dataclasses.asdict(ciphergram)
+        )
 
 
     def test_handle_ciphergram_store_message(self):
-        text = "Message from sender"
-        ciphergram = self.sender_mcrypto.get_ciphergram(
-            self.recver_keys.pub_key_str, text
-        )
-        message = dict(type="ciphergram",**dataclasses.asdict(ciphergram))
         self.receiver = receiver.Receiver(
             Mock(), Mock(), Mock(), self.recver_mcrypto, Mock()
         )
         with patch.object(self.receiver, "_store_as_ciphergram") as mock_sc:
             with patch.object(self.receiver, "_store_as_message") as mock_sm:
-                self.receiver._handle_ciphergram_message("1.1.1.1", message)
+                self.receiver._handle_ciphergram_message("1.1.1.1", self.message)
                 mock_sc.assert_not_called()
                 mock_sm.assert_called()
 
     def test_handle_ciphergram_store_ciphergram(self):
-        text = "Message from sender"
-        ciphergram = self.sender_mcrypto.get_ciphergram(
-            self.recver_keys.pub_key_str, text
-        )
-        message = dict(type="ciphergram",**dataclasses.asdict(ciphergram))
         self.receiver = receiver.Receiver(
             Mock(), Mock(), Mock(), self.sender_mcrypto, Mock()
         )
         with patch.object(self.receiver, "_store_as_ciphergram") as mock_sc:
             with patch.object(self.receiver, "_store_as_message") as mock_sm:
-                self.receiver._handle_ciphergram_message("1.1.1.1", message)
+                self.receiver._handle_ciphergram_message("1.1.1.1", self.message)
                 mock_sc.assert_called()
                 mock_sm.assert_not_called()
 
     def test_handle_ciphergram_dont_store_crypto_error(self):
-        text = "Message from sender"
-        ciphergram = self.sender_mcrypto.get_ciphergram(
-            self.recver_keys.pub_key_str, text
-        )
-        message = dict(type="ciphergram",**dataclasses.asdict(ciphergram))
+        message = self.message.copy()
         message["proof"] = 1
         self.receiver = receiver.Receiver(
             Mock(), Mock(), Mock(), self.recver_mcrypto, Mock()
@@ -75,17 +66,12 @@ class TestReceiver(unittest.TestCase):
                 mock_sm.assert_not_called()
 
     def test_handle_ciphergram_dont_store_expired(self):
-        text = "Message from sender"
-        ciphergram = self.sender_mcrypto.get_ciphergram(
-            self.recver_keys.pub_key_str, text
-        )
-        message = dict(type="ciphergram",**dataclasses.asdict(ciphergram))
         self.receiver = receiver.Receiver(
             Mock(), Mock(), Mock(), self.recver_mcrypto, Mock()
         )
         self.receiver.ttl = 0
         with patch.object(self.receiver, "_store_as_ciphergram") as mock_sc:
             with patch.object(self.receiver, "_store_as_message") as mock_sm:
-                self.receiver._handle_ciphergram_message("1.1.1.1", message)
+                self.receiver._handle_ciphergram_message("1.1.1.1", self.message)
                 mock_sc.assert_not_called()
                 mock_sm.assert_not_called()
