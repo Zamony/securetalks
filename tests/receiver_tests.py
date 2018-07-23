fb90586ed486311076e81fbd0b26648b3c265941
Nikita Mochalov <mochalov.n@yandex.ru>
2018-07-23 12:34:10 +0300

    Made receiver expired message test more concrete

--- tests/receiver_tests.py
+++ tests/receiver_tests.py
@@ -1,3 +1,4 @@
+import time
 import json
 import pprint
 import pathlib
@@ -66,12 +67,20 @@ class TestReceiver(unittest.TestCase):
                 mock_sm.assert_not_called()
 
     def test_handle_ciphergram_dont_store_expired(self):
+        curr_time = time.time()
         self.receiver = receiver.Receiver(
             Mock(), Mock(), Mock(), self.recver_mcrypto, Mock()
         )
         self.receiver.ttl = 0
         with patch.object(self.receiver, "_store_as_ciphergram") as mock_sc:
             with patch.object(self.receiver, "_store_as_message") as mock_sm:
-                self.receiver._handle_ciphergram_message("1.1.1.1", self.message)
-                mock_sc.assert_not_called()
-                mock_sm.assert_not_called()
+                with patch("time.time") as mock_time:
+                    mock_time.return_value = curr_time
+
+                    self.receiver._handle_ciphergram_message(
+                        "1.1.1.1", self.message
+                    )
+
+                    mock_time.assert_called()
+                    mock_sc.assert_not_called()
+                    mock_sm.assert_not_called()
